e657ecedff7f5b41fef5dfc48c3a08047527310e
Peter <peteresiet92@gmail.com>
2022-07-22 02:16:47 +0100

    changed port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
 
-const port = precess.env.PORT || 3000;
+const port = process.env.PORT || 80;
 app.use(
   cors({
     origin: '*',
