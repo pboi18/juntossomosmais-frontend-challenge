57ac064a18e83ac1a6bb7d3ccc92b9edf94d3ded
Peter <peteresiet92@gmail.com>
2022-07-22 19:29:33 +0100

    successfully linked api

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
 'use strict';
+
+// JS For Search Input
+const search = document.getElementById('search');
+search.addEventListener('input', e => {
+  const value = e.target.value;
+  console.log(value);
+});
+// JS For API
+
+const userCardContainer = document.querySelector('[data-user-card-container]');
+const userCardTemplate = document.querySelector('[data-user-template]');
+
+fetch('https://jsm-challenges.herokuapp.com/frontend-challenge.json')
+  .then(res => res.json())
+  .then(data => {
+    const users = Object.values(data);
+    const user = users[0];
+    // console.log(user.length);
+
+    user.forEach(element => {
+      const card = userCardTemplate.content.cloneNode(true).children[0];
+
+      const userCardImage = card.querySelector('[user-image]');
+      const userCardName = card.querySelector('[user-name]');
+      const userCardAdress = card.querySelector('[user-address]');
+      const userCardState = card.querySelector('[user-state]');
+
+      userCardImage.src = element.picture.medium;
+
+      userCardName.textContent = `${element.name.title} ${element.name.first} ${element.name.last}`;
+
+      userCardAdress.textContent = `${element.location.street}`;
+
+      userCardState.textContent = `${element.location.state} ${element.location.city} -CEP: ${element.location.postcode}`;
+
+      userCardContainer.append(card);
+      console.log(card);
+    });
+  });
+
 // JQuery For Pagination
+
 function getPageList(totalPages, page, maxLength) {
   function range(start, end) {
     return Array.from(Array(end - start + 1), (_, i) => i + start);
@@ -36,11 +77,11 @@ function getPageList(totalPages, page, maxLength) {
 }
 
 $(function () {
-  var numberOfItems = $('.card-content .user-card').length;
-  var limitPerPage = 9; //How many card items visible per a page
-  var totalPages = Math.ceil(numberOfItems / limitPerPage);
-  var paginationSize = totalPages; //How many page elements visible in the pagination
-  var currentPage;
+  const numberOfItems = $('.card-content .user-card').length;
+  const limitPerPage = 9; //How many card items visible per a page
+  const totalPages = Math.ceil(numberOfItems / limitPerPage);
+  const paginationSize = totalPages; //How many page elements visible in the pagination
+  let currentPage;
 
   function showPage(whichPage) {
     if (whichPage < 1 || whichPage > totalPages) return false;
@@ -113,32 +154,3 @@ $(function () {
     return showPage(currentPage - 1);
   });
 });
-
-// JS For Search Input
-const search = document.getElementById('search');
-const cardContent = document.getElementById('user-card-container');
-
-// JS For API
-
-const userCardTemplate = document.querySelector('[data-user-template]');
-
-fetch('https://jsm-challenges.herokuapp.com/frontend-challenge.json')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    // data = JSON.stringify(data);
-    // console.log(data);
-    Object.keys(data).forEach(user => {
-      const card = userCardTemplate.content.cloneNode(true).children[0];
-      console.log(user);
-    });
-  });
-
-// .then(data => {
-//   data = JSON.parse(data);
-//   console.log(data);
-//   data.forEach(user => {
-//     const card = userCardTemplate.content.cloneNode(true).children[0];
-//     console.log(user);
-//   });
-// });
